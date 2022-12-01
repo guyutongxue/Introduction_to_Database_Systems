9f4f1bd9612d4854cfad28523715f125977a3766
Guyutongxue <guyutongxue@163.com>
2022-12-01 11:44:42 +0800

    wrong attr

--- packages/frontend/src/components/AdminPanel.vue
+++ packages/frontend/src/components/AdminPanel.vue
@@ -2,12 +2,12 @@
 import RegisterForm from "./RegisterForm.vue";
 
 const showAdminer = import.meta.env.PROD;
-const adminerUrl = `${window.location.hostname}:54188/?pgsql=db`;
+const adminerUrl = `http://${window.location.hostname}:54188/?pgsql=db`;
 </script>
 
 <template>
   <RegisterForm></RegisterForm>
   <VCard v-if="showAdminer" title="Adminer 管理界面" class="mt-3">
-    <iframe :url="adminerUrl" width="500" height="500"></iframe>
+    <iframe :src="adminerUrl" width="500" height="500"></iframe>
   </VCard>
 </template>
